51a3d9cd293e745a93f92e479fcea49dde19d2ec
Marcio Rocha <dev.marcio.rocha@gmail.com>
2022-02-09 23:26:26 -0300

    realizados testes

--- todolist.sh
+++ todolist.sh
@@ -13,15 +13,27 @@ fi
 function listar_arquivos(){
     arquivos=()
     initialFolder="./"
-    if [ -z ne "$1" ] 
-        $initialFolder=
-    for element in $(ls $initialFolder);
+    if ! [ -z "$1" ] ; then
+        initialFolder=$1
+    fi
+    # para todos os arquivos na lista 
+    # TODO -c "Arrumar Lista" -t ARRUMAR
+    for element in $(ls "$initialFolder"); do
+        if [ -f $element ]; then
+            arquivos+=("$element")
+        fi
+    done
+    seq -n /"# TODO"/p $arquivos
 }
 
+# MANIPULACAO DE ARRAY
+# arquivos=()                  # cria o array
+# arquivos+=("readme.md")      # add objeto to array
+# arquivos+=("todolist.sh")    # add objeto to array
+# ${#arquivos[@]}              # retorna o tamanho do array
 
-arquivos=()
 
-for element in $(ls); do
-    if check_file $element
-
-done
+# Manipulação de TODOS com SED
+# cat utils.sh | sed -n '/^# TODO/p' 
+# sed -n '/^# TODO/p' < utils.sh
+# sed -n '/^# TODO/p' < utils.sh >> TodoList ; cat TodoList
