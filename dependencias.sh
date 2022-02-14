3c065fe0c6c06210862dc30c379e9c25eaa8bdb5
Marcio Rocha <dev.marcio.rocha@gmail.com>
2022-02-14 00:29:50 -0300

    adicionado script para configurar as dependencias

---
+++ dependencias.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# TODO -c "Substituir pelo dependencias com argumentos" -t ARRUMAR
+
+utils="https://raw.githubusercontent.com/MarciovsRocha/public-scripts/master/utils.sh"
+
+curl -o utils.sh $utils
