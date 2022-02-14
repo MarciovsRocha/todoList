c43bb4d5f423aa74e9253d0b03c35ad03354fd2c
Marcio Vinicius de Souza da Rocha <marcio.souza@mps.com.br>
2022-02-14 16:21:40 -0300

    realizados novos testes

--- teste.sh
+++ teste.sh
@@ -1,13 +1,40 @@
 #!/bin/bash
 f="/mnt/c/Users/marcio.souza/projetos/montarDiscos"
 d="1"
-mapfile arquivos <<< "$(find "$f" -maxdepth "$d" ! -type d)"
-for arquivo in "${arquivos[@]}"; do
-	echo "--------------------"
-	arquivo="$(tr -d '\n' <<< "$arquivo")"
-	echo "$arquivo"
-	echo "$(sed -e 's/# TODO //' <<< "$(sed -n '/# TODO/p' < "$arquivo")")"
+export=1
+while getopts "f: d: :e" flag; do
+	case "${flag}" in
+		f)
+			f="${OPTARG}"
+			;;
+		d)
+			d=${OPTARG}
+			;;
+		e)
+			export="0"
+			;;
+		*)
+			echo "Parametro '${flag}' incorreto."
+			;;
+	esac
 done
 
+if ! [ -d "$f" ] || [ -z "$f" ] ; then
+	echo "Pasta '$f' inválida."
+else
+	mapfile arquivos <<< "$(find "$f" -maxdepth "$d" ! -type d)"
+	for arquivo in "${arquivos[@]}"; do
+		arquivo="$(tr -d '\n' <<< "$arquivo")"
+		content="$(sed -e 's/# TODO //' <<< "$(sed -n '/# TODO/p' < "$arquivo")")"
+		if ! [ -z "$content" ] ; then
+			if [ "${export}" ] ; then
+				echo '--------------------' >> todoList.txt
+				echo "$arquivo" >> todoList.txt
+				echo "$( tr '\r' '\n' <<< "$content")" >> todoList.txt
+			fi
+			echo -e "--------------------\n${arquivo}\n${content}"
+		fi
+	done
+fi
 
 
